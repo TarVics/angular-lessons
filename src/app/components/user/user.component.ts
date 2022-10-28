ee2d1e0a885b13cdfd597c1d3a3ee7f0c49de0f1
Victor Taran <tarvics@gmail.com>
2022-10-29 01:47:36 +0300

    lesson 2 Routes & state lifting

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+
+import {IUser} from "../../interfaces";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  @Input()
+  user: IUser;
+  @Output()
+  liftUser = new EventEmitter<IUser>();
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  lift(): void {
+    this.liftUser.emit(this.user);
+  }
+
+  getDetails(): Promise<boolean> {
+    // this.router.navigate(['users', this.user.id]);
+    return this.router.navigate([this.user.id], {
+      relativeTo: this.activatedRoute,
+      state: this.user
+    });
+  }
+}
